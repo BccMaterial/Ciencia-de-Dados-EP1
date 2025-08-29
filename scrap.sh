675c312ea869eca5bf214e728abd3f820d4ac631
thiagopls1 <thiagopls1@hotmail.com>
2025-08-29 16:56:40 -0300

    Update `scrap.sh`

--- scrap.sh
+++ scrap.sh
@@ -1,1 +1,5 @@
+echo "Running pokemon spider..."
 scrapy crawl pokemon_spider -o ./output/pokemons.json
+echo "Running ability spider..."
+scrapy crawl ability_spider -o ./output/abilities.json
+echo "Done."
