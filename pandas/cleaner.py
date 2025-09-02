8d5023b729181344c9c2a4c070d30f18bf8f7671
viniciusegydio <egydiovinicius022@gmail.com>
2025-09-02 12:20:33 -0300

    added and changed comments in cleaner.py

--- pandas/cleaner.py
+++ pandas/cleaner.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import json
 
-# Lê o JSON original
+#Lê o arquivo json
 df = pd.read_json("../output/pokemons.json")
 
-# Função auxiliar para limpar listas (remover duplicatas mantendo a ordem)
+#Remove duplicata de listas mantendo a ordem
 def clean_list(x):
-    if isinstance(x, list):
-        seen = set()
+    if isinstance(x, list): #isinstance verifica se evolutions é do tipo list
+        seen = set() #Sets não permitem repetição de elementos e ajudam a fazer a checagem de duplicatas rapidamente
         return [i for i in x if not (i in seen or seen.add(i))]
     return x
 
-# Função auxiliar para limpar evoluções
+
 def clean_evolution(evolutions, name):
-    if isinstance(evolutions, list):
+    if isinstance(evolutions, list): 
         cleaned = []
-        seen = set()
+        seen = set() 
         for evo in evolutions:
             if isinstance(evo, dict):
                 evo_name = evo.get("name")
-                # remove evolução para ele mesmo
+                #Caso contenha evolução para si mesmo, remove
                 if evo_name == name:
                     continue
-                # evita duplicatas
+                
                 evo_tuple = (evo_name, evo.get("variant"))
                 if evo_tuple not in seen:
                     seen.add(evo_tuple)
@@ -30,12 +30,12 @@ def clean_evolution(evolutions, name):
         return cleaned
     return evolutions
 
-# Aplicando as limpezas
+#Limpeza:
 df["types"] = df["types"].apply(clean_list)
 df["abilities"] = df["abilities"].apply(clean_list)
 df["evolution"] = df.apply(lambda row: clean_evolution(row["evolution"], row["name"]), axis=1)
 
-# Exporta para um novo JSON
+#Exporta de data frame para json novamente:
 df.to_json("./pokemons_clean.json", orient="records", indent=2, force_ascii=False)
 
 print("✅ Arquivo 'pokemons_clean.json' gerado com sucesso!")
